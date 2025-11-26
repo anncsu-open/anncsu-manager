32fc59fa9d040fe394ddacfd7ad6dc41788dd9bd
Luigi Pirelli <luipir@gmail.com>
2025-11-26 14:05:25 +0100

    materialize layer in the folder where session is saved (e.g. git clone of remote repo)

--- qgisplugin/anncsu_manager/qgis_plugin_tools/tools/layers.py
+++ qgisplugin/anncsu_manager/qgis_plugin_tools/tools/layers.py
@@ -11,6 +11,7 @@ from qgis.core import QgsVectorLayer, QgsProject, QgsCoordinateReferenceSystem, 
 from qgis.PyQt.QtCore import QVariant
 
 from anncsu_manager.utils.misc_utils import PLUGIN_PATH
+from anncsu_manager.utils.settings_manager import ANNCSUSettingsManager
 
 def remove_layer_by_name(layer_name: str) -> None:
     """Remove a layer from QGIS by its name.
@@ -27,7 +28,8 @@ def load_dataframe_as_layer(
         dataframe: geopandas.GeoDataFrame,
         layer_name: str,
         geometry_column: str = "geometry",
-        crs_epsg: int = 4326) -> QgsVectorLayer:
+        crs_epsg: int = 4326,
+        materialize: bool = True) -> QgsVectorLayer:
     """Load a DataFrame as a QGIS vector layer.
 
     Args:
@@ -66,7 +68,32 @@ def load_dataframe_as_layer(
                 feat.setAttribute(col, str(row[col]))
             provider.addFeature(feat)
 
-    QgsProject.instance().addMapLayer(vl)
+    # save layer to the current Mergin project as parquet file if requested
+    if materialize:
+        session_folder = ANNCSUSettingsManager.get_current_session_folder()
+        if session_folder is None:
+            raise ValueError("No active session found. Please select a session before materializing the layer.")
+
+        output_file_path = session_folder / f"{layer_name}.gpkg"
+
+        # Materialize layer as Parquet file
+        options = QgsVectorFileWriter.SaveVectorOptions()
+        options.fileEncoding = 'UTF-8'
+        options.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteFile
+        options.driverName = 'GPKG'
+        options.layerName = layer_name
+        options.saveMetadata = True
+        options.symbolExport = QgsVectorFileWriter.FeatureSymbology
+
+        write_result, error_message, new_file, new_layer = QgsVectorFileWriter.writeAsVectorFormatV3(
+                    vl,
+                    str(output_file_path),
+                    QgsProject.instance().transformContext(),
+                    options)
+        if write_result != QgsVectorFileWriter.NoError:
+            raise IOError(f"Error saving layer '{layer_name}' to file '{output_file_path}': {error_message}")
+        else:
+            vl = QgsVectorLayer(str(output_file_path), layer_name, "ogr")
 
     # apply related style if exists
     named_style_path = Path(PLUGIN_PATH) / "resources" / "styles" / f"{layer_name}_style.qml"
@@ -75,5 +102,8 @@ def load_dataframe_as_layer(
         vl.loadNamedStyle(str(named_style_path))
     else:
         print(f"Style file not found: {named_style_path}")
+
+    # show the layer in QGIS
+    QgsProject.instance().addMapLayer(vl)
 
     return vl
